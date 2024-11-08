f030e268ca5b11752b21630a5a74deed95ba0ff9
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-11-08 08:07:01 -0500

    Update Z_Open_appts_aging_30.sql
    Added TRIM functions for qualifying fields.

--- Z_Open_appts_aging_30.sql
+++ Z_Open_appts_aging_30.sql
@@ -1,28 +1,27 @@
 -- Service calls
 SELECT
-	C.Service_Call_ID AS woNumber
-	,a.Appointment
-	,c.Status_of_Call
-	,c.Type_of_Call
-	,c.ADRSCODE AS LocationCode
-	,c.LOCATNNM AS LocationName
-	,C.Service_Description AS woDescription
-	,c.Technician_Team
-	,c.Technician_ID
-	,c.Technician_ID2
-	,c.Technician
-	,c.Resolution_ID
-	,c.Resolution_Description
-	,c.Date_of_Service_Call
+	TRIM(c.Service_Call_ID) AS WO_Number
+	,TRIM(a.Appointment) AS Appt_Number
+	,TRIM(c.Status_of_Call) AS WO_Status
+	,TRIM(c.Type_of_Call) AS WO_Source
+	,TRIM(c.ADRSCODE) AS Location_Code
+	,TRIM(c.LOCATNNM) AS Location_Name
+	,TRIM(C.Service_Description) AS WO_Description
+	,TRIM(c.Technician_Team) AS Service_Team
+	,TRIM(c.Technician_ID) AS Technician_1
+	,TRIM(c.Technician) AS Technician_2
+	,TRIM(c.Resolution_ID) AS Resolution_Code
+	,TRIM(c.Resolution_Description) AS Resolution_Description
+	,c.Date_of_Service_Call AS WO_Date
 	,a.Task_Date AS Date_Appt
 	,a.Completion_Date AS Date_Complete_Appt
 	,c.Completion_Date AS Date_Complete_WO
-	,c.DATE1
-	,c.Contract_Number
-	,c.Divisions
-	,c.CUSTNMBR AS CustomerCode
-	,c.CUSTNAME AS CustomerName
-	,c.Bill_Customer_Number
+	,c.DATE1 AS WO_Created_Date
+	,TRIM(c.Contract_Number) AS Contract_Number
+	,TRIM(c.Divisions) AS Division
+	,TRIM(c.CUSTNMBR) AS Customer_Code
+	,TRIM(c.CUSTNAME) AS Customer_Name
+	,(c.Bill_Customer_Number) AS Customer_Bill_Code
 	--,*
 FROM
 	SV00300 AS c
